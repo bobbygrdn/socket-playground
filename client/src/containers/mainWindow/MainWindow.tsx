df60e9c26254d51f74725c2e25e95f0b83ca992f
bobbygrdn <bobbygrdn777@gmail.com>
2024-08-01 10:53:42 -0700

    Adds browserRouter to handle component routing for the context window.

--- client/src/containers/mainWindow/MainWindow.tsx
+++ client/src/containers/mainWindow/MainWindow.tsx
@@ -1,12 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ContextWindow } from "../contextWindow/ContextWindow";
 import { Header } from '../header/Header';
 import { Sidebar } from "../sidebar/Sidebar";
+import { Conversation } from '../../components/conversation/Conversation';
+import { GroupConversation } from '../../components/groupConversation/GroupConversation';
+import { Settings } from '../../components/settings/Settings';
 import './MainWindow.css';
 
 export const MainWindow: React.FC = (): JSX.Element => (
-    <>
+    <Router>
         <Header />
         <Sidebar />
-        <ContextWindow />
-    </>
+        <Routes>
+            <Route path="/" element={<ContextWindow />}>
+                <Route path="conversations/:conversationId" element={<Conversation />} />
+                <Route path="groupConversations/:groupId" element={<GroupConversation />} />
+                <Route path="settings" element={<Settings />} />
+            </Route>
+        </Routes>
+    </Router>
 );
