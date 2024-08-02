c94d9265899c840192c0a70300b8e7a4f4089faf
bobbygrdn <bobbygrdn777@gmail.com>
2024-08-01 17:16:08 -0700

    Adds space for easier readability

--- client/src/containers/contextWindow/ContextWindow.tsx
+++ client/src/containers/contextWindow/ContextWindow.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 import './ContextWindow.css';
 
 export const ContextWindow: React.FC = (): JSX.Element => {
+
     return (
         <article id="contextWindow">
             <Outlet />
