177db96d42336ef3f50b7d69aced27ee9e3d19a1
bobbygrdn <bobbygrdn777@gmail.com>
2024-08-01 17:15:12 -0700

    Adds the chatProvider to the root

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,6 +1,10 @@
 import { MainWindow } from './containers/mainWindow/MainWindow';
+import { ChatProvider } from './context/ChatContext';
+
 export const App: React.FC = (): JSX.Element => {
     return (
-        <MainWindow />
+        <ChatProvider>
+            <MainWindow />
+        </ChatProvider>
     );
 }
