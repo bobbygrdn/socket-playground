ef5018c207a2e31798f97357e00f51a326633b27
bobbygrdn <bobbygrdn777@gmail.com>
2024-08-01 20:00:04 -0700

    Creates util for connecting to mongoDB database

---
+++ server/src/mongoDatabase.ts
@@ -0,0 +1,17 @@
+import mongoose, { Mongoose } from 'mongoose';
+
+export const mongoDatabase = {
+    initConnetion: async (): Promise<Mongoose> => {
+        try {
+            const { MONGODB_CONNECTION_STRING } = process.env;
+            console.log("Mongo DB Connection String: ", MONGODB_CONNECTION_STRING);
+            if (!MONGODB_CONNECTION_STRING) {
+                throw new Error('MONGODB_CONNECTION_STRING is undefined');
+            }
+            return await mongoose.connect(MONGODB_CONNECTION_STRING);
+        } catch (err) {
+            console.error('Error connecting to db: ', err);
+            throw err;
+        }
+    },
+};
