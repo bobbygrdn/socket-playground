d07f1ea1b3daef1d73e6b7a4b7e065f1a2830b86
bobbygrdn <bobbygrdn777@gmail.com>
2024-08-06 10:43:07 -0700

    Adds logic to create a socket connection to the server

--- client/src/containers/contextWindow/ContextWindow.tsx
+++ client/src/containers/contextWindow/ContextWindow.tsx
@@ -1,7 +1,20 @@
 import { Outlet } from 'react-router-dom';
+import { io, Socket } from 'socket.io-client';
+import { useEffect } from 'react';
 import './ContextWindow.css';
 
+let socket: Socket;
+const ENDPOINT = 'http://localhost:5000';
+
 export const ContextWindow: React.FC = (): JSX.Element => {
+
+    useEffect(() => {
+        socket = io(ENDPOINT);
+        socket.on("connected", () => console.log("Server Connected"))
+        return () => {
+            socket.disconnect();
+        };
+    }, []);
 
     return (
         <article id="contextWindow">
