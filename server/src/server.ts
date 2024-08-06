dcae98c0faf76349ad23cc8ca854b757cf57b0e7
bobbygrdn <bobbygrdn777@gmail.com>
2024-08-06 10:42:25 -0700

    Updates logic to use error handler middleware, create httpServer, and setup a socket.io connection to the client

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,26 +1,44 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { Server } from 'socket.io';
 import { userRouter } from '../src/user/userRouter';
 import { messageRouter } from '../src/message/messageRouter';
 import { chatRouter } from '../src/chat/chatRouter';
+import { healthCheckRouter } from '../src/healthCheck/healthCheckRouter';
+import errorHandler from '../src/common/middleware/errorHandler';
+import { createServer } from 'http';
 
 const app = express();
 dotenv.config();
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:8080" }));
 app.use(express.json());
 
-app.get("/", (res: Response) => {
-    res.send("API is running");
-});
-
+app.use("/", healthCheckRouter);
 app.use("/api/users", userRouter);
 app.use("/api/messages", messageRouter);
 app.use("/api/chat", chatRouter);
 
+app.use(errorHandler());
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const httpServer = createServer(app);
+
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:8080",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log(`New client connected on socket: ${socket.id}`);
+    socket.emit("connected");
+});
+
+httpServer.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
-})
+});
