2919a7bf9181267f61ba219672e34c76695fae72
bobbygrdn <bobbygrdn777@gmail.com>
2024-08-06 10:51:52 -0700

    Updates logic to refer to chats with many people in them to channels instead of groupChats

--- client/src/context/ChatContext.tsx
+++ client/src/context/ChatContext.tsx
@@ -8,36 +8,25 @@ interface User {
 interface Chat {
     _id: string;
     chatName: string;
-    isGroupChat: boolean;
+    isChannel: boolean;
     users: User[];
 }
 
 interface ChatContextType {
     chats: Chat[];
     setChats: React.Dispatch<React.SetStateAction<Chat[]>>;
-    groupChats: Chat[];
-    setGroupChats: React.Dispatch<React.SetStateAction<Chat[]>>;
+    channels: Chat[];
+    setChannels: React.Dispatch<React.SetStateAction<Chat[]>>;
 }
 
 const ChatContext = createContext<ChatContextType | undefined>(undefined);
 
 const ChatProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-    const [groupChats, setGroupChats] = useState<Chat[]>([]);
+    const [channels, setChannels] = useState<Chat[]>([]);
     const [chats, setChats] = useState<Chat[]>([]);
 
-    useEffect(() => {
-        fetch('http://localhost:5000/api/chats')
-            .then(res => res.json())
-            .then(data => {
-                const groupChats = data.filter((chat: Chat) => chat.isGroupChat);
-                const individualChats = data.filter((chat: Chat) => !chat.isGroupChat);
-                setGroupChats(groupChats);
-                setChats(individualChats);
-            });
-    }, []);
-
     return (
-        <ChatContext.Provider value={{ chats, setChats, groupChats, setGroupChats }}>
+        <ChatContext.Provider value={{ chats, setChats, channels, setChannels }}>
             {children}
         </ChatContext.Provider>
     );
