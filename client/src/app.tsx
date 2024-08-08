527bd45490bfc901a2e320be3c9aba23f5ef2da0
bobbygrdn <bobbygrdn777@gmail.com>
2024-08-08 11:40:42 -0700

    Adds the userprovider to the root of the app

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -3,17 +3,20 @@ import { Authentication } from './containers/authentication/Authentication';
 import { Login } from './containers/login/Login';
 import { Register } from './containers/register/Register';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
 
 export const App: React.FC = (): JSX.Element => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Authentication />}>
-                    <Route path="login" element={<Login />} />
-                    <Route path="register" element={<Register />} />
-                </Route>
-                <Route path="/home/*" element={<MainWindow />} />
-            </Routes>
-        </Router>
+        <UserProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Authentication />}>
+                        <Route path="login" element={<Login />} />
+                        <Route path="register" element={<Register />} />
+                    </Route>
+                    <Route path="/home/*" element={<MainWindow />} />
+                </Routes>
+            </Router>
+        </UserProvider>
     );
 }
