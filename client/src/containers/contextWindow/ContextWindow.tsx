0e513100813e75bc20702193553f943ec5b60ba3
bobbygrdn <bobbygrdn777@gmail.com>
2024-08-08 11:44:15 -0700

    Adds type assertion operator to check for null variables and adds logic to fetch channels and direct messages

--- client/src/containers/contextWindow/ContextWindow.tsx
+++ client/src/containers/contextWindow/ContextWindow.tsx
@@ -1,12 +1,47 @@
 import { Outlet } from 'react-router-dom';
 import { io, Socket } from 'socket.io-client';
-import { useEffect } from 'react';
+import { useEffect, useContext } from 'react';
+import { UserContext } from '../../context/UserContext';
+import { ChatContext } from '../../context/ChatContext';
 import './ContextWindow.css';
 
 let socket: Socket;
 const ENDPOINT = 'http://localhost:5000';
 
+interface User {
+    name: string;
+    email: string;
+}
+
+interface Chat {
+    _id: string;
+    chatName: string;
+    isChannel: boolean;
+    users: User[];
+}
+
 export const ContextWindow: React.FC = (): JSX.Element => {
+
+    const userContext = useContext(UserContext);
+    const chatContext = useContext(ChatContext);
+
+    useEffect(() => {
+        const fetchChats = async () => {
+            try {
+                const response = await fetch(`${ENDPOINT}/api/chats/${userContext?.user?._id}`);
+                const data = await response.json();
+
+                if (!data) return;
+
+                chatContext?.setChats(data?.responseObject?.filter((channel: Chat) => channel.isChannel === false));
+                chatContext?.setChannels(data?.responseObject?.filter((channel: Chat) => channel.isChannel === true));
+            } catch (error) {
+                console.error('Error fetching chats: ', error);
+            }
+        };
+
+        fetchChats();
+    }, [])
 
     useEffect(() => {
         socket = io(ENDPOINT);
