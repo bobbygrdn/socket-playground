38640bcc06cac0b4cdc4ee0061ce30ec89a8e1f8
bobbygrdn <bobbygrdn777@gmail.com>
2024-08-09 13:57:01 -0700

    Adds the route to create a conversation

--- client/src/containers/mainWindow/MainWindow.tsx
+++ client/src/containers/mainWindow/MainWindow.tsx
@@ -3,6 +3,7 @@ import { ContextWindow } from "../contextWindow/ContextWindow";
 import { Header } from '../header/Header';
 import { Sidebar } from "../sidebar/Sidebar";
 import { Conversation } from '../../components/conversation/Conversation';
+import { CreateConversation } from '../../components/createConversation/CreateConversation';
 import { Settings } from '../../components/settings/Settings';
 import './MainWindow.css';
 import { ChatProvider } from '../../context/ChatContext';
@@ -14,6 +15,7 @@ export const MainWindow: React.FC = (): JSX.Element => (
         <Routes>
             <Route path="/" element={<ContextWindow />}>
                 <Route path="conversations/:conversationId" element={<Conversation />} />
+                <Route path="create-conversation" element={<CreateConversation />} />
                 <Route path="settings" element={<Settings />} />
             </Route>
         </Routes>
