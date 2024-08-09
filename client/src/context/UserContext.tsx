94d5106a0c0d357dcdb67d9aa897b0138e2ceb84
bobbygrdn <bobbygrdn777@gmail.com>
2024-08-09 10:10:34 -0700

    Adds logic to check if the render is the initial render and if it is not then wait until the complete render has happened before routing using a useRef hook

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useState, useEffect, ReactNode } from 'react';
+import React, { createContext, useState, useEffect, ReactNode, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface User {
     _id: string | null;
@@ -8,14 +9,35 @@ interface User {
 }
 
 interface UserContextType {
-    user: User | undefined;
-    setUser: React.Dispatch<React.SetStateAction<User | undefined>>;
+    user: User | null;
+    setUser: React.Dispatch<React.SetStateAction<User | null>>;
 }
 
 const UserContext = createContext<UserContextType | undefined>(undefined);
 
 const UserProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-    const [user, setUser] = useState<User | undefined>(undefined);
+    const [user, setUser] = useState<User | null>(null);
+    const navigate = useNavigate();
+    const isInitialRender = useRef(true);
+
+    useEffect(() => {
+        const userInfo = localStorage.getItem("userInfo");
+        setUser(userInfo ? JSON.parse(userInfo) : null);
+    }, [])
+
+
+    useEffect(() => {
+        if (isInitialRender.current) {
+            isInitialRender.current = false;
+            return;
+        }
+
+        if (!user) {
+            navigate('/');
+        } else {
+            navigate('/home');
+        }
+    }, [user]);
 
     return (
         <UserContext.Provider value={{ user, setUser }}>
