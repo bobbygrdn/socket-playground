66387ebb724266f3897a1e7ae6ea841b93ed9454
bobbygrdn <bobbygrdn777@gmail.com>
2024-08-09 09:49:40 -0700

    Removes groupConversation component logic and route

--- client/src/containers/mainWindow/MainWindow.tsx
+++ client/src/containers/mainWindow/MainWindow.tsx
@@ -3,7 +3,6 @@ import { ContextWindow } from "../contextWindow/ContextWindow";
 import { Header } from '../header/Header';
 import { Sidebar } from "../sidebar/Sidebar";
 import { Conversation } from '../../components/conversation/Conversation';
-import { GroupConversation } from '../../components/groupConversation/GroupConversation';
 import { Settings } from '../../components/settings/Settings';
 import './MainWindow.css';
 import { ChatProvider } from '../../context/ChatContext';
@@ -15,7 +14,6 @@ export const MainWindow: React.FC = (): JSX.Element => (
         <Routes>
             <Route path="/" element={<ContextWindow />}>
                 <Route path="conversations/:conversationId" element={<Conversation />} />
-                <Route path="groupConversations/:groupId" element={<GroupConversation />} />
                 <Route path="settings" element={<Settings />} />
             </Route>
         </Routes>
