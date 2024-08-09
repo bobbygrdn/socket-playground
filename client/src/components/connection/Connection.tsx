77540a287866f6772baeb60824a3a288b02e0010
bobbygrdn <bobbygrdn777@gmail.com>
2024-08-09 09:48:19 -0700

    Adds a specific id to each direct message list item

--- client/src/components/connection/Connection.tsx
+++ client/src/components/connection/Connection.tsx
@@ -8,7 +8,7 @@ interface ConnectionProps {
 
 export const Connection: React.FC<ConnectionProps> = ({ profileImage, user, userId }) => {
     return (
-        <li className="direct-message" key={user}>
+        <li className="direct-message" id={userId} key={user}>
             <Link to={`/home/conversations/${userId}`} style={{ textDecoration: 'none', color: 'inherit' }}>
                 <figure style={{ display: 'flex', gap: '10px', padding: '5px 0' }}>
                     <img src={profileImage} alt={user} style={{ borderRadius: '50%', width: '30px', height: '30px' }} />
